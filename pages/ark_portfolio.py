8a3121b045fe3a58b224dee89e649e28538fea43
David L.E <59020919+neldivad@users.noreply.github.com>
2022-03-01 01:48:10 +0800

    Update ark_portfolio.py

--- pages/ark_portfolio.py
+++ pages/ark_portfolio.py
@@ -81,8 +81,12 @@ def app():
     test_df = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSJ5B9E5p6MbKot2HBwAwkGGr_YxVJWgUdTgUVvamEtI6Vo2IdsqcjUq-MCdVoJD7dYpawtaHxgfSNO/pub?output=csv") 
     st.dataframe(test_df)
     
-    df = make_df(spreadsheet_id, 'Daily ARK data').astype(str)
-    st.write(df)
+#     df = make_df(spreadsheet_id, 'Daily ARK data').astype(str)
+#     st.write(df)
+    
+    df = make_df(spreadsheet_id, 'Daily ARK data')
+    st.dataframe(df)
+    
     st.download_button(
         label='Click to download CSV file', 
         data= convert_df(df), 
