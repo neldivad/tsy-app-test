f2631fc4f5c5dbf854ea72fcc6174a07b3f29f12
David L.E <59020919+neldivad@users.noreply.github.com>
2022-03-01 15:13:48 +0800

    Update corr.py

--- pages/corr.py
+++ pages/corr.py
@@ -123,7 +123,8 @@ def calcular_correlacoes():
       #st.write(correlacao_tempo)
 #       fig = correlacao_tempo.iplot(asFigure=True, xTitle='Data', yTitle='Correlação %',
 #                                    title='Correlação no Tempo entre os Ativos e ' + indice)
-      fig = px.scatter(correlacao_tempo, x= 'Data', y= 'Correlação %')
+      st.write(correlacao_tempo)
+      fig = px.line(correlacao_tempo, x= 'Data', y= 'Correlação %')
     
       st.plotly_chart(fig)
 
