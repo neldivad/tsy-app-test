42bad1a6586edd1cf5912b1e88a6754ab8c93d9e
David L.E <59020919+neldivad@users.noreply.github.com>
2022-03-01 15:22:02 +0800

    Update corr.py

--- pages/corr.py
+++ pages/corr.py
@@ -121,7 +121,7 @@ def calcular_correlacoes():
       correlacao_tempo.dropna(inplace=True)
       correlacao_tempo.drop(columns=['IBOV','SP500','Dolar'], inplace=True)
       st.write(correlacao_tempo)
-      fig = correlacao_tempo.plot(asFigure=True, 
+      fig = correlacao_tempo.iplot(asFigure=True, 
                                   xTitle='Data', yTitle='Correlação %',
                                   title='Correlação no Tempo entre os Ativos e ' + indice)
       # fig = px.line(correlacao_tempo, x= 'Data', y= 'Correlação %')
