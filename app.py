a43758dd899d3a15fb4a856dba06aa792bee9936
David L.E <59020919+neldivad@users.noreply.github.com>
2022-03-10 02:50:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,52 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit_player import st_player
+from streamlit_option_menu import option_menu
+
+import pandas as pd
 
 # Custom imports 
-from multipage import MultiPage
-from pages import home, pr, ark_portfolio, test, corr
+from pages import home, fundamentals, ark_portfolio #, pr, , test, corr
 
 st.set_page_config(
      page_title="Ticker Symbol YOU | Channel that invests in you",
-     page_icon="https://cdn-icons-png.flaticon.com/512/2617/2617909.png",
+     page_icon="https://www.qed-insights.com/content/images/2021/04/qed-mark-black---600.png",
      layout="wide",
      initial_sidebar_state="expanded",
  )
 
-st.sidebar.text("> Pre-Alpha version 1.1.1")
-with st.sidebar:
-     components.html("""
-     <a href="https://twitter.com/just_neldivad" class="twitter-follow-button" data-show-screen-name="true" data-show-count="true">Follow @just_neldivad</a>
-     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-     """, height=30,)
+def main():
+    # Removing and add pages
+    pages = {
+        "Wassuppppp": homepage,
+        'Fundamentals': fundamental_page,
+        'Cathie\'s Portfolio': cathie_portfolio,
+    }
+
+    st.sidebar.write(' ')
+    with st.sidebar:
+        st.title('App Navigation')
+        page = option_menu("", tuple(pages.keys()), 
+            menu_icon="list", default_index=0)
+
+    pages[page]()
+
+    with st.sidebar.expander('Version'):
+        st.write('Streamlit:', st.__version__)
+        st.write('Pandas:', pd.__version__)
+
+    with st.sidebar:
+        st.markdown('''<small>QED Insights</small>''', unsafe_allow_html=True)
+        st.markdown('''<small>dl.eeee.nv@gmail.com</small>''', unsafe_allow_html=True)
     
-# Create an instance of the app 
-app = MultiPage()
+def homepage():
+    home.home()
 
-st.title("TSY")
-st.subheader("Random text here so I know where I put it")
-# Add all your application here
-app.add_page("Wassup", home.app)
-app.add_page("PR", pr.app)
-app.add_page("ARK Invest Portfolio", ark_portfolio.app)
-app.add_page("Download yfinance", corr.app)
+def fundamental_page():
+    fundamentals.fundamentals()
 
-# app.add_page("DATA STRUCTURES", data_structures.app)
-# app.add_page("PROBLEMS", problems.app)
+def cathie_portfolio():
+    ark_portfolio.app()
 
-# The main app
-app.run() 
+if __name__ == "__main__":
+    main()
